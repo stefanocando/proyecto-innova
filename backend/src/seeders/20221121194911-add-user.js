024c2b095ac0a56024546d644e1603f25fa273f3
JOSE PLAZA <jplazam@uc.cl>
2022-11-21 23:29:09 -0300

    sssssssssssss

--- backend/src/seeders/20221121194911-add-user.js
+++ backend/src/seeders/20221121194911-add-user.js
@@ -1,13 +1,18 @@
 'use strict';
+const bcrypt = require('bcrypt');
+const stef_password = "stef"
+const allan_password = "alla"
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
+    const stef_hpassword = await bcrypt.hash(stef_password, 5);
+    const allan_hpassword = await bcrypt.hash(allan_password, 5);
     return queryInterface.bulkInsert("Users", [
       {
         username: "Stefano",
         telephone: "+56986753456",
-        password: "stefano",
+        password: stef_hpassword,
         categories: ["Vegetariano", "Bajo En Azucar", "Sin Gluten"],
         createdAt: new Date(),
         updatedAt: new Date(),
@@ -15,7 +20,7 @@ module.exports = {
       {
         username: "Allan",
         telephone: "+56987657689",
-        password: "allan",
+        password: allan_hpassword,
         categories: ["Vegetariano", "Sin Sodio"],
         createdAt: new Date(),
         updatedAt: new Date(),
